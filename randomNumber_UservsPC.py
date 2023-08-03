4faffec304387fe9e524a964dadd77f746ce5b27
beelyya <huonghtq15@gmail.com>
2023-08-03 13:13:23 +0700

    Create

---
+++ randomNumber_UservsPC.py
@@ -0,0 +1,13 @@
+from random import randint
+
+user_choice = int(input("Choose number:"))
+pc_choice = randint(1,100)
+
+if user_choice == pc_choice:
+    print("You won")
+elif user_choice > pc_choice:
+    print("Lower! Computer choose:", pc_choice)
+elif user_choice < pc_choice:
+    print("Higher! Computer choose:", pc_choice)
+else:
+    print("hi")
